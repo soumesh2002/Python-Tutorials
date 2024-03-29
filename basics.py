7f809391b4cbba94fa717dcc455c4024f0bbb825
soumesh2002 <soumesh6622@gmail.com>
2024-03-29 16:54:05 +0530

    Initial Commit

---
+++ basics.py
@@ -0,0 +1,8 @@
+# -------------------------- #
+# --------- BASICS --------- #
+def check_score(marks: int):
+    return marks
+
+
+marks_in = int(input())
+print(f"Sir, I scored: {check_score(marks_in)} out of 30")
